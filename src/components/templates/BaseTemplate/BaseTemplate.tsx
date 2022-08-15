2834951164ebd27115a4b7fd753abb2eb589f93b
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-15 17:47:33 +0300

    add scroll event listener

--- src/components/templates/BaseTemplate/BaseTemplate.tsx
+++ src/components/templates/BaseTemplate/BaseTemplate.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import styles from "./BaseTemplate.module.css";
 
 const BaseTemplate = () => {
+  useEffect(() => {
+    const scroll = (event: Event) => {
+      console.log(event);
+    };
+    window.addEventListener("scroll", scroll);
+
+    return () => window.removeEventListener("scroll", scroll);
+  }, []);
+
   return (
     <div className={styles.container}>
       <header>
