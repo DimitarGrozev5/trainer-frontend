08db62970ac4385718de6079ae7d5928dffa1daf
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-15 17:43:35 +0300

    Setup Route for not logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import BaseTemplate from "./components/templates/BaseTemplate/BaseTemplate";
 
 function App() {
-  return <div>Test react app</div>;
+  const accessToken = null;
+  return (
+    <Routes>
+      {!accessToken && (
+        <Route path="/" element={<BaseTemplate />}>
+          <Route index element={<div>Test react app</div>} />
+        </Route>
+      )}
+    </Routes>
+  );
 }
 
 export default App;
