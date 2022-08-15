ef43ca6d625f078931d1ec2db5578540b66a5f42
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-15 16:42:28 +0300

    Wrap App in store Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import store from "./redux-store/index";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
