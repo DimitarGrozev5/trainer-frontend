8ead6d7d0d37a9cfde33e05087d57b232405dfb5
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-16 16:22:45 +0300

    Orginize Input Component

--- src/components/UI-elements/Input/Input.tsx
+++ src/components/UI-elements/Input/Input.tsx
@@ -4,6 +4,7 @@ import { nanoid } from "nanoid";
 import styles from "./Input.module.css";
 import { useTState } from "../../../hooks/useTState";
 
+// Props type definitions
 interface Props {
   label: string;
   type: "text" | "email" | "password";
@@ -28,54 +29,64 @@ const Input: React.FC<Props> = ({
   const [showPassword, , { toggleState: toggleShowPassword }] =
     useTState(false);
 
-  // Setup class names
+  // Setup class names for input
   const classNames = [styles["text-input"]];
 
+  // Add invalid class if field is invalid
   const invalid = error && error.length;
   invalid && classNames.push(styles.error);
 
+  // Create handlers for onChange and onBlur
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
     onChange(e.currentTarget.value);
-
   const blurHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
     onBlur && onBlur(e.currentTarget.value);
 
-  return (
-    <div className={`${styles.container} ${invalid ? styles.error : ""}`}>
-      <label className={styles.label} htmlFor={id}>
-        {label}
-      </label>
-      {type === "password" && (
-        <div className={styles.password}>
-          <input
-            className={classNames.join(" ")}
-            type={showPassword ? "text" : "password"}
-            id={id}
-            name={id}
-            value={value}
-            onChange={changeHandler}
-            onBlur={blurHandler}
-          />
-          <button
-            type="button"
-            onClick={toggleShowPassword}
-            className={styles.password}
-          >
-            {showPassword ? "Hide" : "Show"}
-          </button>
-        </div>
-      )}
-      {type !== "password" && (
+  // Create Component for text or email Input
+  let input = (
+    <input
+      className={classNames.join(" ")}
+      type={type}
+      id={id}
+      name={id}
+      value={value}
+      onChange={changeHandler}
+      onBlur={blurHandler}
+    />
+  );
+
+  // Create Component for password input
+  if (type === "password") {
+    input = (
+      <div className={styles.password}>
         <input
           className={classNames.join(" ")}
-          type={type}
+          type={showPassword ? "text" : "password"}
           id={id}
           name={id}
           value={value}
           onChange={changeHandler}
           onBlur={blurHandler}
         />
-      )}
+        <button
+          type="button"
+          onClick={toggleShowPassword}
+          className={styles.password}
+        >
+          {showPassword ? "Hide" : "Show"}
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className={`${styles.container} ${invalid ? styles.error : ""}`}>
+      <label className={styles.label} htmlFor={id}>
+        {label}
+      </label>
+
+      {input}
+
       {invalid && <div className={styles["error-box"]}>{error}</div>}
     </div>
   );
