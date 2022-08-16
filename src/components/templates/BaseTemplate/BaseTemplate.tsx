2e3f765a3bfe1497d335ba5c18337d86377efe03
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-16 08:28:26 +0300

    Basic layout animation achieved

--- src/components/templates/BaseTemplate/BaseTemplate.tsx
+++ src/components/templates/BaseTemplate/BaseTemplate.tsx
@@ -1,21 +1,62 @@
-import { useEffect } from "react";
+import { useEffect, useId, useRef, useState } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { Outlet } from "react-router-dom";
 import styles from "./BaseTemplate.module.css";
 
 const BaseTemplate = () => {
-  useEffect(() => {
-    const scroll = (event: Event) => {
-      console.log(event);
-    };
-    window.addEventListener("scroll", scroll);
+  // Get ref to container div
+  const containerRef: React.Ref<HTMLDivElement> = useRef(null);
+  const h1Ref: React.Ref<HTMLDivElement> = useRef(null);
+  const h3Ref: React.Ref<HTMLDivElement> = useRef(null);
 
-    return () => window.removeEventListener("scroll", scroll);
-  }, []);
+  // Get current scroll position
+  const { scrollYProgress } = useScroll({
+    target: h1Ref,
+    offset: ["start start", "end start"],
+  });
+
+  // Animate headers based on scroll position
+  const cutoff = 0.35;
+  const fadeOutAnim = useTransform(
+    scrollYProgress,
+    [0, cutoff, 0.5, 1],
+    [1, 0, 0, 0]
+  );
+  const yOutAnim = useTransform(
+    scrollYProgress,
+    [0, cutoff, 0.5, 1],
+    ["0vh", "5vh", "0vh", "0vh"]
+  );
+  const fadeInAnim = useTransform(
+    scrollYProgress,
+    [0, cutoff, 0.5, 1],
+    [0, 0, 1, 1]
+  );
+
+  scrollYProgress.onChange((latest) => {
+    if (!h3Ref.current) {
+      return;
+    }
+    if (latest >= 1) {
+      h3Ref.current.style.position = "fixed";
+      h3Ref.current.style.left = "0.5rem";
+      return;
+    }
+    if (latest < 0.88) {
+      h3Ref.current.style.position = "relative";
+      h3Ref.current.style.left = "0px";
+    }
+  });
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={containerRef}>
       <header>
-        <h1>Trainer</h1>
+        <motion.h1 ref={h1Ref} style={{ opacity: fadeOutAnim, y: yOutAnim }}>
+          Trainer
+        </motion.h1>
+        <motion.h3 ref={h3Ref} style={{ opacity: fadeInAnim }} layout>
+          Trainer
+        </motion.h3>
       </header>
       <main>
         <Outlet />
