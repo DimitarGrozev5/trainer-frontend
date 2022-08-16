e3dc94d9893870e9e4a93199bb7e001cc255d226
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-16 10:09:48 +0300

    change component to Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import BaseTemplate from "./components/templates/BaseTemplate/BaseTemplate";
+import Card from "./components/UI-elements/Card/Card";
 
 function App() {
   const accessToken = null;
@@ -8,7 +9,7 @@ function App() {
     <Routes>
       {!accessToken && (
         <Route path="/" element={<BaseTemplate />}>
-          <Route index element={<div>Test react app</div>} />
+          <Route index element={<Card>Test react app</Card>} />
         </Route>
       )}
     </Routes>
