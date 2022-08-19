96a45f1e2115a379b74af40fcb552b90bb47ac38
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-19 13:44:42 +0300

    Add Calendar component to TrainingHub

--- src/components/route-endpoints/TrainingHub/TrainingHub.tsx
+++ src/components/route-endpoints/TrainingHub/TrainingHub.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+import Calendar from "../../Calendar/Calendar";
 import Card from "../../UI-elements/Card/Card";
 import styles from "./TrainingHub.module.css";
 
 const TrainingHub = () => {
+  // State for controlling the selected day
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <>
-      <Card className={styles.calendar}>Calendar</Card>
+      <Card className={styles.calendar}>
+        <Calendar selectedDate={selectedDate} onChangeDate={setSelectedDate} />
+      </Card>
       <Card className={styles.today}>Today</Card>
     </>
   );
