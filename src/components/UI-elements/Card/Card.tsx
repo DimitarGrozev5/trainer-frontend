b555ff3df266e8e7991d48da642ba71c4b83f117
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-19 12:02:28 +0300

    Modify Card to accept className

--- src/components/UI-elements/Card/Card.tsx
+++ src/components/UI-elements/Card/Card.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import styles from "./Card.module.css";
 
 interface Props {
+  className?: string;
   children: React.ReactNode;
 }
 
-const Card: React.FC<Props> = ({ children }) => {
-  return <div className={styles.card}>{children}</div>;
+const Card: React.FC<Props> = ({ className, children }) => {
+  const classes = [styles.card];
+  className && classes.push(className);
+
+  return <div className={classes.join(" ")}>{children}</div>;
 };
 
 export default Card;
