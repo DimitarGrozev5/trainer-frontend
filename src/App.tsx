cda2db635113ccee3eeeaa87c5964eb0a388f596
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-19 12:02:13 +0300

    Added Route for TrainingHub

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./components/route-endpoints/HomePage/HomePage";
+import TrainingHub from "./components/route-endpoints/TrainingHub/TrainingHub";
 import BaseTemplate from "./components/templates/BaseTemplate/BaseTemplate";
-import { useAppSelector } from "./hooks/redux-hooks";
 import { useAuth } from "./hooks/useAuth";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<BaseTemplate />}>
         {!token && <Route index element={<HomePage />} />}
-        {!!token && <Route index element={<div>Logged in</div>} />}
+        {!!token && <Route index element={<TrainingHub />} />}
       </Route>
     </Routes>
   );
