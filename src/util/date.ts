0d0ef9dc2c8a33ddba8ebd43efe627182b965b0d
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-21 00:07:43 +0300

    create getMonth function

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,5 +1,5 @@
+// Get month name
 export const getMonthName = (date: Date) => {
-  // Get month name
   return [
     "January",
     "February",
@@ -16,6 +16,7 @@ export const getMonthName = (date: Date) => {
   ][date.getMonth()];
 };
 
+// Check if two dates are the same, ignoring hours, minutes, seconds and miliseconds
 export const sameDate = (date1: Date, date2: Date): boolean => {
   return (
     date1.getFullYear() === date2.getFullYear() &&
@@ -24,9 +25,49 @@ export const sameDate = (date1: Date, date2: Date): boolean => {
   );
 };
 
+// Check if two dates are in the same month
 export const sameMonth = (date1: Date, date2: Date): boolean => {
   return (
     date1.getFullYear() === date2.getFullYear() &&
     date1.getMonth() === date2.getMonth()
   );
+};
+
+// Generate an array of the days of a month, based on a Date from that month
+export const getMonthArr = (
+  date: Date,
+  { numberOfWeeks = 6, skipDaysFromOtherMonths = false } = {}
+): Date[][] => {
+  // Get first day of month
+  const firstDay: Date = new Date(date.getTime());
+  firstDay.setMilliseconds(0);
+  firstDay.setSeconds(0);
+  firstDay.setMinutes(0);
+  firstDay.setHours(0);
+  firstDay.setDate(1);
+
+  // Get day of week
+  const firstDayOfWeek: number = firstDay.getDay();
+
+  // Get date of monday
+  const mondayDateUTC: DateUTC =
+    firstDay.getTime() - firstDayOfWeek * 24 * 60 * 60 * 1000;
+
+  // Generate array for month
+  type DateUTC = number;
+  const month: Date[][] = [];
+  for (let week = 0; week < numberOfWeeks; week++) {
+    const weekArr: Date[] = [];
+    for (let day = 0; day < 7; day++) {
+      const today = new Date(
+        mondayDateUTC +
+          week * 7 * 24 * 60 * 60 * 1000 +
+          day * 24 * 60 * 60 * 1000
+      );
+      weekArr.push(today);
+    }
+    month.push(weekArr);
+  }
+
+  return month;
 };
