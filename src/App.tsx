5ebb32bd2fa6bf22a8044b2d7e4ff816f6dae08a
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-21 14:36:11 +0300

    Add Logout route

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import HomePage from "./components/route-endpoints/HomePage/HomePage";
+import Logout from "./components/route-endpoints/Logout/Logout";
 import TrainingHub from "./components/route-endpoints/TrainingHub/TrainingHub";
 import BaseTemplate from "./components/templates/BaseTemplate/BaseTemplate";
 import { useAuth } from "./hooks/useAuth";
 
 function App() {
   // Get authorization data from localStorage
-  // const token = useAuth().token;
-  const token = true;
+  const token = useAuth().token;
+  // const token = true;
 
   return (
     <Routes>
       <Route path="/" element={<BaseTemplate />}>
         {!token && <Route index element={<HomePage />} />}
-        {!!token && <Route index element={<TrainingHub />} />}
+        {!!token && (
+          <>
+            <Route index element={<TrainingHub />} />
+          </>
+        )}
       </Route>
+
+      {!!token && <Route path="/logout" element={<Logout />} />}
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
---
+++ src/components/route-endpoints/Logout/Logout.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux-hooks";
+import { useHttpClient } from "../../../hooks/useHttpClient";
+import { userActions } from "../../../redux-store/userSlice";
+import LoadingSpinner from "../../UI-elements/LoadingSpinner/LoadingSpinner";
+
+const Logout = () => {
+  const { isLoading, sendRequest } = useHttpClient();
+
+  const dispatch = useAppDispatch();
+  const userId = useAppSelector((state) => state.user.userId);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await sendRequest(`/users/${userId}/logout`, { method: "POST" });
+      } catch (err) {
+        console.log(err);
+      }
+      dispatch(userActions.clearUserData());
+    })();
+  }, [sendRequest]);
+
+  return (
+    <>
+      {isLoading && <LoadingSpinner asOverlay />}
+      <div
+        style={{
+          position: "fixed",
+          left: "0px",
+          right: "0px",
+          top: "0px",
+          bottom: "0px",
+          zIndex: -1,
+          backgroundColor: "#fff",
+        }}
+      ></div>
+    </>
+  );
+};
+
+export default Logout;
