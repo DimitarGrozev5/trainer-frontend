73532bf0fb6eb547268b488153d2fb84aa878cc9
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-21 18:55:18 +0300

    Add headings to page

--- src/components/route-endpoints/ManagePrograms/ManagePrograms.tsx
+++ src/components/route-endpoints/ManagePrograms/ManagePrograms.tsx
@@ -1,7 +1,19 @@
+import Card from "../../UI-elements/Card/Card";
 import styles from "./ManagePrograms.module.css";
 
 const ManagePrograms = () => {
-  return <div>Programs</div>;
+  // 
+
+  return (
+    <>
+      <Card>
+        <h1 className={styles.h1}>Your Programs:</h1>
+      </Card>
+      <Card>
+        <h1 className={styles.h1}>Add a new program:</h1>
+      </Card>
+    </>
+  );
 };
 
 export default ManagePrograms;
