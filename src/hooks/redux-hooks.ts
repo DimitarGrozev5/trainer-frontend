2cf0448276663ad905e4a461f869c6724e3bc487
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-22 07:55:58 +0300

    Add function that returns an array of programs

--- src/hooks/redux-hooks.ts
+++ src/hooks/redux-hooks.ts
@@ -4,3 +4,6 @@ import type { RootState, AppDispatch } from "../redux-store/index";
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const populateProgramsArr = () => (state: RootState) =>
+  state.programs.arr.map((id) => state.programs.byId[id]);
