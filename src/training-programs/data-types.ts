2f973b2f503df0acf8302f66e96333eea24f83a4
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-22 13:10:46 +0300

    add init props to TrainingProgram

--- src/training-programs/data-types.ts
+++ src/training-programs/data-types.ts
@@ -1,5 +1,12 @@
+import React from "react";
+
 // All valid programIds will be stored here
 export type ProgramId = "EnduroGrip" | "Q&D";
+
+interface InitProps {
+  onSubmit: (initData: Object) => void;
+}
+export type InitComponent = React.FC<InitProps>;
 
 export interface TrainingProgram {
   id: ProgramId;
@@ -8,4 +15,7 @@ export interface TrainingProgram {
   name: string;
   shortDesc: string;
   longDesc: string;
+
+  InitComponent: InitComponent;
+  initValues: (initVals: Object) => Object;
 }
