87dd5886b951ead0cecb1d02400746207fe06833
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-22 16:58:55 +0300

    Update parent value when settings change

--- src/training-programs/enduro-grip/enduro-grip.tsx
+++ src/training-programs/enduro-grip/enduro-grip.tsx
@@ -3,6 +3,7 @@ import { useForm } from "../../hooks/useForm/useForm";
 import Input from "../../components/UI-elements/Input/Input";
 import { useSState } from "../../hooks/useSState";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export const enduroGrip: TrainingProgram = {
   // Basic data
@@ -19,6 +20,16 @@ export const enduroGrip: TrainingProgram = {
   InitComponent: ({ value, onChange }) => {
     const [startToday, setStartToday] = useState(true);
     const [startDate, setStartDate] = useState(new Date());
+
+    // Reset startDate to today when startToday is false
+    useEffect(() => {
+      !startToday && setStartDate(new Date());
+    }, [startToday]);
+
+    // Update value when settings change
+    useEffect(() => {
+      value !== startDate && onChange({ startDate });
+    }, [startDate]);
 
     return (
       <>
