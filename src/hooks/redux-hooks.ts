849130cdf8d40bedc6cc3c5bd8aae4c4f970c2e4
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-22 09:04:33 +0300

    change populateProgramsArr to return TrainingProgram[]

--- src/hooks/redux-hooks.ts
+++ src/hooks/redux-hooks.ts
@@ -1,9 +1,17 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "../redux-store/index";
+import { programs } from "../training-programs";
+import { TrainingProgram } from "../training-programs/data-types";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const populateProgramsArr = () => (state: RootState) =>
-  state.programs.arr.map((id) => state.programs.byId[id]);
+  state.programs.arr.map(
+    (id) =>
+      ({
+        ...state.programs.byId[id],
+        ...programs.get(id),
+      } as TrainingProgram)
+  );
