bd6e796a1f71f54c2a510d9caa8f308095969ea7
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-23 12:02:40 +0300

    Createda a confirm modal

---
+++ src/components/UI-elements/Modal/ConfirmModal.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "../Button/Button";
+import Modal from "./Modal";
+
+interface Props {
+  show: boolean;
+  message: string;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+const ErrorModal: React.FC<Props> = ({ show, message, onClose, onConfirm }) => {
+  return (
+    <Modal
+      show={show}
+      title="Confirm"
+      onClose={onClose}
+      buttons={
+        <>
+          <Button onClick={onClose} plain>
+            Cancel
+          </Button>
+          <Button onClick={onConfirm} plain>
+            Confirm
+          </Button>
+        </>
+      }
+    >
+      {message}
+    </Modal>
+  );
+};
+
+export default ErrorModal;
