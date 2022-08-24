25e63a95427c2f6038a2c3a928f0aa0d2a02e09c
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-24 11:32:42 +0300

    Sanitize input data for scheduleService

--- src/hooks/ScheduleService/training-schedule-types.ts
+++ src/hooks/ScheduleService/training-schedule-types.ts
@@ -1,3 +1,12 @@
-export interface ScheduleService {
-  
+export interface ScheduledSession {
+  name: string;
+  shortDesc: string;
+  params: any;
 }
+
+export type DailySchedule = ScheduledSession[];
+
+export type ScheduleService = (
+  startDate: Date,
+  endDate: Date
+) => Map<Date, DailySchedule>;
--- src/hooks/ScheduleService/useScheduleService.ts
+++ src/hooks/ScheduleService/useScheduleService.ts
@@ -1,8 +1,54 @@
-import { useMemo } from "react";
-import { ScheduleService } from "./training-schedule-types";
+import { compareAsc } from "date-fns";
+import { useCallback } from "react";
+import { roundDate } from "../../util/date";
+import { populateProgramsArr, useAppSelector } from "../redux-hooks";
+import {
+  DailySchedule,
+  ScheduleService,
+  ScheduledSession,
+} from "./training-schedule-types";
 
 export const useScheduleService = (): ScheduleService => {
-  const scheduleService = useMemo(() => ({} as ScheduleService), []);
+  // Get all active programs
+  const activePrograms = useAppSelector(populateProgramsArr()).filter(
+    (p) => p.active
+  );
+
+  const scheduleService = useCallback(
+    (startDate: Date, endDate: Date) => {
+      const schedule = new Map<Date, DailySchedule>();
+
+      const now = new Date();
+
+      //// Adjust start and end dates
+      let sd = startDate;
+      let ed = endDate;
+
+      // Start date must be before end date
+      if (compareAsc(startDate, endDate) >= 0) {
+        return schedule;
+      }
+
+      // Start date must be after end date
+      if (compareAsc(startDate, now) > 0) {
+        sd = now;
+      }
+
+      // End date must be after today
+      if (compareAsc(now, endDate) > 0) {
+        ed = now;
+      }
+
+      //// Remove hours minutes and seconds from start and end dates
+      sd = roundDate(sd);
+      ed = roundDate(ed);
+
+      //// Loop from start to end date and populate them
+
+      return schedule;
+    },
+    [activePrograms]
+  );
 
   return scheduleService;
 };
