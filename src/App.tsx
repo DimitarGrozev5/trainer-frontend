206a3535c47c69618f0a14a608474c6137951096
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-25 10:49:32 +0300

    Add schedule cache context

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Logout from "./components/route-endpoints/Logout/Logout";
 import ManagePrograms from "./components/route-endpoints/ManagePrograms/ManagePrograms";
 import TrainingHub from "./components/route-endpoints/TrainingHub/TrainingHub";
 import BaseTemplate from "./components/templates/BaseTemplate/BaseTemplate";
+import ScheduleCacheProvider from "./hooks/ScheduleService/schedule-cache-context";
 import { useAuth } from "./hooks/useAuth";
 
 function App() {
@@ -13,20 +14,22 @@ function App() {
   // const token = true;
 
   return (
-    <Routes>
-      <Route path="/" element={<BaseTemplate />}>
-        {!token && <Route index element={<HomePage />} />}
-        {!!token && (
-          <>
-            <Route index element={<TrainingHub />} />
-            <Route path="/manage-programs" element={<ManagePrograms />} />
-          </>
-        )}
-      </Route>
+    <ScheduleCacheProvider>
+      <Routes>
+        <Route path="/" element={<BaseTemplate />}>
+          {!token && <Route index element={<HomePage />} />}
+          {!!token && (
+            <>
+              <Route index element={<TrainingHub />} />
+              <Route path="/manage-programs" element={<ManagePrograms />} />
+            </>
+          )}
+        </Route>
 
-      {!!token && <Route path="/logout" element={<Logout />} />}
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+        {!!token && <Route path="/logout" element={<Logout />} />}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </ScheduleCacheProvider>
   );
 }
 
