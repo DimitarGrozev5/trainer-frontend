56ec698cbdec9cd49c5a8ff60572b236062c28fe
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-08-25 12:04:41 +0300

    modify populateProgramsArr to optionaly filter programs

--- src/hooks/redux-hooks.ts
+++ src/hooks/redux-hooks.ts
@@ -2,18 +2,35 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "../redux-store/index";
 import { programs } from "../training-programs";
 import { ProgramId, TrainingProgram } from "../training-programs/data-types";
+import { ProgramsState } from "../redux-store/programsSlice";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export const populateProgramsArr = () => (state: RootState) =>
-  state.programs.arr.map((id) => {
-    return {
-      ...programs.get(id),
-      ...state.programs.byId[id],
-    } as TrainingProgram;
-  });
+export const populateProgramsArr =
+  (option: boolean | null = null) =>
+  (state: RootState) => {
+    // Filter programs based on option
+    // True - active
+    // False - !active
+    if (option !== null) {
+      return state.programs.arr.flatMap((id) => {
+        return state.programs.byId[id].active === option
+          ? ({
+              ...programs.get(id),
+              ...state.programs.byId[id],
+            } as TrainingProgram)
+          : [];
+      });
+    }
+    return state.programs.arr.map((id) => {
+      return {
+        ...programs.get(id),
+        ...state.programs.byId[id],
+      } as TrainingProgram;
+    });
+  };
 
 export const populateProgram = (id: ProgramId) => (state: RootState) => {
   return {
