75d448025e617f4e599300a5d46830e8d1ae155d
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-26 08:03:36 +0300

    CountdownTimer boilerplate

---
+++ src/training-programs/common-components/CountdownTimer/CountdownTimer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./CountdownTimer.module.css";
+
+interface Props {}
+
+const CountdownTimer: React.FC<Props> = () => {
+  return <div>Timer</div>;
+};
+
+export default CountdownTimer;
