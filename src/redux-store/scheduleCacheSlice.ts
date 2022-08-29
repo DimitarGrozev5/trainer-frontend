adb4e5cd87c4dd9d3073b3f524eaffb1ea4eeab2
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-29 21:00:56 +0300

    Absurdly long middleware probably caches dates

--- src/redux-store/scheduleCacheSlice.ts
+++ src/redux-store/scheduleCacheSlice.ts
@@ -1,7 +1,9 @@
 import { createSlice, Middleware } from "@reduxjs/toolkit";
 import { PayloadAction } from "@reduxjs/toolkit";
+import { programs } from "../training-programs";
 
-import { ProgramId } from "../training-programs/data-types";
+import { ProgramId, TrainingProgram } from "../training-programs/data-types";
+import { ProgramState } from "./programsSlice";
 
 export class ScheduledSession {
   constructor(
@@ -14,8 +16,16 @@ export class ScheduledSession {
 }
 
 // Redux type
+export type ScheduledDate = {
+  [date: number]: ScheduledSession | null;
+};
 export type ScheduleCacheState = {
-  [programId in ProgramId]: ScheduledSession[];
+  [programId in ProgramId]: ScheduledDate;
+};
+
+type Payload = {
+  program: ProgramId;
+  dates: ScheduledDate;
 };
 
 const scheduleCacheSlice = createSlice({
@@ -24,6 +34,12 @@ const scheduleCacheSlice = createSlice({
   initialState: {} as ScheduleCacheState,
 
   reducers: {
+    addToCache: (state, action: PayloadAction<Payload>) => {
+      state = { ...state, [action.payload.program]: action.payload.dates };
+    },
+    removeProgram: (state, action: PayloadAction<ProgramId>) => {
+      delete state[action.payload];
+    },
     clearCache: (state) => {
       state = {} as ScheduleCacheState;
     },
@@ -34,14 +50,79 @@ export const scheduleCacheActions = scheduleCacheSlice.actions;
 export const scheduleCacheReducer = scheduleCacheSlice.reducer;
 
 export const scheduleCacheMiddleware: Middleware =
-  (storeAPI) => (next) => (action) => {
-    switch (action.type) {
-      case "programs/updateProgramsState":
-        console.log(storeAPI);
-        break;
-
-      default:
-        break;
+  ({ getState, dispatch }) =>
+  (next) =>
+  (action) => {
+    if (action.type !== "programs/updateProgramsState") {
+      return next(action);
     }
+
+    action.payload.forEach((p: ProgramState) => {
+      // If the program is changed to inactive, remove it from the cache
+      if (!p.active) {
+        dispatch(scheduleCacheActions.removeProgram(p.id));
+      }
+
+      // Get full program data
+      const fullPr = programs.get(p.id);
+      if (!fullPr) {
+        return;
+      }
+      const program: TrainingProgram = { ...fullPr, ...p };
+
+      // Init cahce for program
+      let cache = {} as ScheduledDate;
+
+      //// Create a cache for the next three months
+      const startDate = program.state.sessionDate;
+      const endDate = startDate + 3 * 30 * 24 * 60 * 60 * 1000;
+
+      let nextSession: ScheduledDate = {
+        [startDate]: new ScheduledSession(
+          program.id,
+          program.name,
+          program.shortDesc,
+          program.getDescFromState(program.state),
+          program.state
+        ),
+      };
+      cache = { ...cache, ...nextSession };
+      let nextState = program.getNextState(
+        program.state,
+        {},
+        { forceProgress: true, fromToday: false }
+      );
+
+      for (let i = startDate; i < endDate; i += 24 * 60 * 60 * 1000) {
+        let today: ScheduledDate = {
+          [i]: null,
+        };
+
+        if (i === nextState.sessionDate) {
+          today = {
+            [startDate]: new ScheduledSession(
+              program.id,
+              program.name,
+              program.shortDesc,
+              program.getDescFromState(program.state),
+              program.state
+            ),
+          };
+
+          nextState = program.getNextState(
+            nextState,
+            {},
+            { forceProgress: true, fromToday: false }
+          );
+        }
+
+        cache = { ...cache, ...today };
+      }
+
+      dispatch(
+        scheduleCacheActions.addToCache({ program: p.id, dates: cache })
+      );
+    });
+
     return next(action);
   };
