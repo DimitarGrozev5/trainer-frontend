510156d3976a4b2532273b08179ab566081b96c9
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-09-09 13:04:03 +0300

    comments

--- src/training-programs/data-types.ts
+++ src/training-programs/data-types.ts
@@ -4,12 +4,16 @@ import {
   EnduroGripInit,
   EnduroGripState,
 } from './enduro-grip/enduro-grip-types';
-import { eesAchieved, eesInit, eesState } from './even-easier-strength/ees-types';
+import {
+  eesAchieved,
+  eesInit,
+  eesState,
+} from './even-easier-strength/ees-types';
 
 // All valid programIds will be stored here
 export type ProgramId = 'EnduroGrip' | 'ees';
 
-// All valid program states wll be stored here
+// Type maps for the different programs
 type ProgramStateMap = {
   EnduroGrip: EnduroGripState;
   ees: eesState;
@@ -23,6 +27,7 @@ type ProgramAchievedMap = {
   ees: eesAchieved;
 };
 
+// Init Props and Init Component data types
 export interface InitProps<id extends ProgramId> {
   value: ProgramInitMap[id];
   onChange: (initData: ProgramInitMap[id]) => void;
@@ -30,12 +35,14 @@ export interface InitProps<id extends ProgramId> {
 
 export type InitComponent<id extends ProgramId> = React.FC<InitProps<id>>;
 
+// Session Props and Session Component data types
 export interface SessionProps<id extends ProgramId> {
   program: TrainingProgram<id>;
   onAchievedChanged: (val: ProgramAchievedMap[id]) => void;
 }
 export type SessionComponent<id extends ProgramId> = React.FC<SessionProps<id>>;
 
+// Training Program data type
 export interface TrainingProgram<id extends ProgramId> {
   id: id;
   active: boolean;
