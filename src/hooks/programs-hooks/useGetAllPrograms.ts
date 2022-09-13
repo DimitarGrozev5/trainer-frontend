264d06c74b13c0ebe60b40a1e4196e582a14f82e
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-09-13 20:11:43 +0300

    create hook to get all programs

---
+++ src/hooks/programs-hooks/useGetAllPrograms.ts
@@ -0,0 +1,36 @@
+import { useMemo } from 'react';
+import { programs } from '../../training-programs';
+import { ProgramId, TrainingProgram } from '../../training-programs/data-types';
+import { useAppSelector } from '../redux-hooks';
+
+export const useGetAllPrograms = () => {
+  const programsIds = useAppSelector((state) => state.programs.arr);
+  const programsObj = useAppSelector((state) => state.programs.byId);
+
+  const allPrograms = useMemo(
+    () =>
+      programsIds.flatMap((id) => {
+        if (!programs.has(id) || !(id in programsObj)) {
+          return [];
+        }
+
+        const programMethods = programs.get(id)!;
+        const programData = programsObj[id];
+
+        return { ...programMethods, ...programData };
+      }),
+    [programsIds, programsObj]
+  );
+
+  const activePrograms = useMemo(
+    () => allPrograms.filter((pr) => pr.active),
+    [allPrograms]
+  );
+
+  const inactivePrograms = useMemo(
+    () => allPrograms.filter((pr) => !pr.active),
+    [allPrograms]
+  );
+
+  return { allPrograms, activePrograms, inactivePrograms };
+};
