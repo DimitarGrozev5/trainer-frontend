351d773b2173dd8815cb0e21a8446d5a4eae36a7
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-09-14 23:26:56 +0300

    endurogrip adapted to new methods

--- src/training-programs/enduro-grip/enduro-grip.tsx
+++ src/training-programs/enduro-grip/enduro-grip.tsx
@@ -114,12 +114,10 @@ export const enduroGrip: TP<'EnduroGrip', true> = {
 
   getNextState: (
     prevState: EnduroGripState,
-    achieved: EnduroGripAchieved | null,
-    { forceProgress = false, fromToday = true } = {
-      forceProgress: false,
-      fromToday: true,
-    }
+    achieved: EnduroGripAchieved | 'skip'
   ): EnduroGripState => {
+    const skip = achieved === 'skip';
+
     const {
       sessionDate: sessionDateUtc,
       sessionIndex,
@@ -128,29 +126,42 @@ export const enduroGrip: TP<'EnduroGrip', true> = {
       currentScheduleIndex,
     } = prevState;
 
+    /// Calculate next session date
+    // Convert sessionDate to Date object
     const sessionDate = new Date(sessionDateUtc);
 
-    const trainingPlan = new CircularArray(trainingRotation, sessionIndex);
-
+    // Convert schedule to CircularArray
     const schedulePlan = new CircularArray<number>(
       schedule,
       currentScheduleIndex
     );
 
-    const fromDate = fromToday ? new Date() : sessionDate;
+    // Select the base date
+    const fromDate = skip ? sessionDate : new Date();
+
+    // Calculate next session date
     const nextSessionDate = roundDate(
       add(fromDate, { days: schedulePlan.i(0) })
     );
 
+    // Calculate next schedule index
     const nextScheduleIndex = schedulePlan.getIndex(+1);
 
+    /// Calculate next session params
+    // Extract achived sets and fallback to lastHeavySessionAchieved if *skip*
+    const sets = skip ? lastHeavySessionAchieved : achieved.sets;
+
+    // Convert training rotation to CircularArray
+    const trainingPlan = new CircularArray(trainingRotation, sessionIndex);
+
+    // Set variables
     let nextSessionIndex = sessionIndex;
-    let heavySessionAchieved = achieved?.sets || 0;
+    let heavySessionAchieved = sets;
 
     // If the session was heavy always move to the next session and record the achieved result
     if (sessionIndex % 2 === 0) {
       nextSessionIndex = trainingPlan.getIndex(+1);
-      heavySessionAchieved = achieved?.sets || 0;
+      heavySessionAchieved = sets;
     }
 
     // If the session was light decide wheter to move to the next session or the previous and don't record the achieved result
@@ -158,11 +169,63 @@ export const enduroGrip: TP<'EnduroGrip', true> = {
       heavySessionAchieved = lastHeavySessionAchieved;
 
       // If the last heavy session the user achieved the required number of sets, progress
-      if (forceProgress || lastHeavySessionAchieved === trainingPlan.i(-1)) {
+      if (lastHeavySessionAchieved === trainingPlan.i(-1)) {
         nextSessionIndex = trainingPlan.getIndex(+1);
       } else {
         nextSessionIndex = trainingPlan.getIndex(-1);
       }
+    }
+
+    return {
+      sessionDate: nextSessionDate.getTime(),
+      sessionIndex: nextSessionIndex,
+      lastHeavySessionAchieved: heavySessionAchieved,
+      schedule,
+      currentScheduleIndex: nextScheduleIndex,
+    };
+  },
+  getNextScheduleState: (prevState: EnduroGripState): EnduroGripState => {
+    const {
+      sessionDate: sessionDateUtc,
+      sessionIndex,
+      schedule,
+      currentScheduleIndex,
+    } = prevState;
+
+    /// Calculate next session date
+    // Convert sessionDate to Date object
+    const sessionDate = new Date(sessionDateUtc);
+
+    // Convert schedule to CircularArray
+    const schedulePlan = new CircularArray<number>(
+      schedule,
+      currentScheduleIndex
+    );
+
+    // Calculate next session date
+    const nextSessionDate = roundDate(
+      add(sessionDate, { days: schedulePlan.i(0) })
+    );
+
+    // Calculate next schedule index
+    const nextScheduleIndex = schedulePlan.getIndex(+1);
+
+    /// Calculate next session params
+    // Convert training rotation to CircularArray
+    const trainingPlan = new CircularArray(trainingRotation, sessionIndex);
+
+    // Set variables
+    let nextSessionIndex = sessionIndex;
+    const heavySessionAchieved = 0;
+
+    // If the session was heavy always move to the next session and record the achieved result
+    if (sessionIndex % 2 === 0) {
+      nextSessionIndex = trainingPlan.getIndex(+1);
+    }
+
+    // If the session was light decide wheter to move to the next session or the previous and don't record the achieved result
+    else {
+      nextSessionIndex = trainingPlan.getIndex(+1);
     }
 
     return {
