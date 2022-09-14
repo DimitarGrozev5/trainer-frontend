2e138d484f7b1c17a2d4700a3c274f25a91d2336
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-09-14 10:38:16 +0300

    make return types explicit

--- src/hooks/programs-hooks/useGetAllPrograms.ts
+++ src/hooks/programs-hooks/useGetAllPrograms.ts
@@ -1,12 +1,13 @@
 import { useMemo } from 'react';
 import { programs } from '../../training-programs';
+import { ProgramId, TP } from '../../training-programs/data-types';
 import { useAppSelector } from '../redux-hooks';
 
 export const useGetAllPrograms = () => {
   const programsIds = useAppSelector((state) => state.programs.arr);
   const programsObj = useAppSelector((state) => state.programs.byId);
 
-  const allPrograms = useMemo(
+  const allPrograms: TP<ProgramId, boolean>[] = useMemo(
     () =>
       programsIds.flatMap((id) => {
         if (!programs.has(id) || !(id in programsObj)) {
@@ -21,12 +22,12 @@ export const useGetAllPrograms = () => {
     [programsIds, programsObj]
   );
 
-  const activePrograms = useMemo(
+  const activePrograms: TP<ProgramId, true>[] = useMemo(
     () => allPrograms.filter((pr) => pr.active),
     [allPrograms]
   );
 
-  const inactivePrograms = useMemo(
+  const inactivePrograms: TP<ProgramId, false>[] = useMemo(
     () => allPrograms.filter((pr) => !pr.active),
     [allPrograms]
   );
