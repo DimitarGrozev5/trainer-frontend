c531ddbe7c581f0c9ca555f48aaf39409083f478
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-09-14 11:35:59 +0300

    Add getProgram method to hook

--- src/hooks/programs-hooks/useGetAllPrograms.ts
+++ src/hooks/programs-hooks/useGetAllPrograms.ts
@@ -1,12 +1,14 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { programs } from '../../training-programs';
-import { ProgramId, TP } from '../../training-programs/data-types';
+import { ProgramId, TP, TPState } from '../../training-programs/data-types';
 import { useAppSelector } from '../redux-hooks';
 
 export const useGetAllPrograms = () => {
+  // Get program data from redux
   const programsIds = useAppSelector((state) => state.programs.arr);
   const programsObj = useAppSelector((state) => state.programs.byId);
 
+  // Function to get all programs with methods
   const allPrograms: TP<ProgramId, boolean>[] = useMemo(
     () =>
       programsIds.flatMap((id) => {
@@ -22,15 +24,31 @@ export const useGetAllPrograms = () => {
     [programsIds, programsObj]
   );
 
+  // Function to get active programs with methods
   const activePrograms: TP<ProgramId, true>[] = useMemo(
     () => allPrograms.filter((pr) => pr.active),
     [allPrograms]
   );
 
+  // Function to get inactive programs with methods
   const inactivePrograms: TP<ProgramId, false>[] = useMemo(
     () => allPrograms.filter((pr) => !pr.active),
     [allPrograms]
   );
 
-  return { allPrograms, activePrograms, inactivePrograms };
+  // Function to get a specific program with methods
+  const getProgram = useCallback<
+    (id: ProgramId) => TP<ProgramId, boolean> | null
+  >((id: ProgramId) => {
+    if (!programs.has(id) || !(id in programsObj)) {
+      return null;
+    }
+
+    const programMethods = programs.get(id)!;
+    const programData = programsObj[id];
+
+    return { ...programMethods, ...programData };
+  }, []);
+
+  return { allPrograms, activePrograms, inactivePrograms, getProgram };
 };
