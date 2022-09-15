2d1898619a72d06db1add3f7c3ca90cefccc8a3d
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-09-15 09:57:42 +0300

    use thunk in TrainigHub skip

--- src/components/route-endpoints/TrainingHub/TrainingHub.tsx
+++ src/components/route-endpoints/TrainingHub/TrainingHub.tsx
@@ -42,14 +42,21 @@ const TrainingHub = () => {
 
     const nextState = program.getNextState(program.state, 'skip');
 
-    dispatch(
-      programsActions.update({
-        id: program.id,
-        state: nextState,
-        achieved: { force: true },
-        version: program.version,
-      })
-    );
+    // dispatch(
+    //   programsActions.update({
+    //     id: program.id,
+    //     state: nextState,
+    //     achieved: { force: true },
+    //     version: program.version,
+    //   })
+    // );
+
+    dispatch(programsActions.updateThunk({
+      id: program.id,
+      state: nextState,
+      version:program.version,
+      'skip',
+    }));
   };
 
   return (
